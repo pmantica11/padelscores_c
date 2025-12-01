42ecdfb5f07ca056771bfb8c90ebe246a63d2e56
Pedro Mantica <pedro@helius.xyz>
2025-12-01 14:49:16 +0000

    Nit

--- streamlit_app.py
+++ streamlit_app.py
@@ -137,7 +137,7 @@ try:
     ratings_df['Title'] = ratings_df['Player'].map(titles)
     ratings_df = ratings_df[['Player', 'Title', 'Rating']]
     ratings_df = ratings_df.set_index('Title')
-    st.dataframe(ratings_df, use_container_width=True, height="stretch")
+    st.dataframe(ratings_df, height="stretch")
 
 except Exception as e:
     st.error(f"Error: {str(e)}")
